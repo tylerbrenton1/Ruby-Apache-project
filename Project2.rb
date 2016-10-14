056c5e1386bdcbd933b6836a49a1f9c1537fc34c
tylerbrenton1 <tyler.brenton1@tamu.edu>
2016-10-13 21:01:40 -0500

    Final comit before big change

--- Project2.rb
+++ Project2.rb
@@ -1,20 +1,22 @@
+puts "Now downloading file."
 require 'open-uri'
 open('http_access_log', 'wb') do |file|
   file << open('http://s3.amazonaws.com/tcmg412-fall2016/http_access_log').read
 end
+puts "Now reading file."
 apache_logs = File.readlines "http_access_log"
-
 def parse(logs) 
 
   apache_regex = /\A(?<ip_address>\S+) \S+ \S+ \[(?<time>[^\]]+)\] "(?<method>GET|POST) (?<url>\S+) \S+?" (?<status>\d+) (?<bytes>\S+)/
 
-  result_parse = []
+ result_parse = []
   logs.each do |log|
     parser = log.scan(apache_regex)[0]
 
     # If can't parse the log line for any reason.
     if log.scan(apache_regex)[0].nil?
-       next
+     
+      next
     end
 
     parse = 
@@ -35,15 +37,20 @@ def parse(logs)
 
   return result_parse
 end 
-
-require 'pp'
-pp parse(apache_logs)
-
-
-
+puts "Now spliting data."
+log = []
+log << parse(apache_logs)
+#/(:[u][r]._[a-z]{4}=>)"\d{3}"/
 
 
+print "#{log}"
 
+
+
+count = 0
+File.open("http_access_log") {|f| count = f.read.count("\n")}
+
+puts "#{count}"
 
 
 
