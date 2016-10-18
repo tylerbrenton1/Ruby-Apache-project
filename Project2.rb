aeba0f5d2b8b2d16f5538711be90518d7b4fdc94
tylerbrenton1 <tyler.brenton1@tamu.edu>
2016-10-18 16:17:07 -0500

    Percents added

--- Project2.rb
+++ Project2.rb
@@ -4,7 +4,7 @@ open('http_access_log', 'wb') do |file|
   file << open('http://s3.amazonaws.com/tcmg412-fall2016/http_access_log').read
 end
 puts "Now doing a thing."
-
+#This pulls out the date
 dlog = File.readlines "http_access_log"
 def parse(logs) 
 
@@ -32,6 +32,7 @@ end
 date_log = []
 date_log << parse(dlog)
 puts "Now doing another thing."
+#This pulls out the files requested
 flog = File.readlines "http_access_log" 
 def parse(logs) 
 
@@ -58,7 +59,10 @@ def parse(logs)
 end 
 file_log = []
 file_log << parse(flog)
-puts "Now doing someother ting."
+file_name = file_log.map{|x|x.values}.flatten.inspect
+
+#This pulls out the respones code
+puts "Now doing some other thing."
 clog = File.readlines "http_access_log"
 def parse(logs) 
 
@@ -83,30 +87,41 @@ def parse(logs)
 
   return code
 end
-code_log = []
-code_log << parse(clog)
+finished_code_parse = (parse(clog))
+codes_only = finished_code_parse.map{|x|x.values}.flatten.inspect
+tot_codes = codes_only.count('/...,/') # keep this
+puts "#{tot_codes} This should equal all total counts and need to be removed once done"
+#This handels the 400 codes
+fourcodes = codes_only.count('/4../')
+#This handels the 300 codes
+threecodes = codes_only.count('/3../')
+#this handels 200 codes
+twocodes = codes_only.count('/2../')
 
 
-#/(:[u][r]._[a-z]{4}=>)"\d{3}"/
+class Numeric
+  def percent_of(n)
+    self.to_f / n.to_f * 100.0
+  end
+end
+Puts "ALL INFO BELOW"
 
 
-print "#{file_log}"
-
-
-
-count = 0
-File.open("http_access_log") {|f| count = f.read.count("\n")}
-
-puts "#{count}"
-
+puts "300 count need to be removed once done #{threecodes}"
+puts "200 count  need to be removed once done#{twocodes}"
+puts "400 count need to be removed once done #{fourcodes}"
+fourpercent = fourcodes.percent_of(tot_codes).round
+puts " The percent of error request is #{fourpercent}."
+threepercent = threecodes.percent_of(tot_codes).round
+puts " The percent of redirect request is #{threepercent}."
 
 
 
 
   #Once you download the file, you will be parsing the file in order to answer several questions:
-#How many total requests were made in the time period represented in the log?
-#How many requests were made on each day?
-#What percentage of the requests were not successful (any 4xx status code)?
-#What percentage of the requests were redirected elsewhere (any 3xx codes)?
+#How many total requests were made in the time period represented in the log? DONE
+#How many requests were made on each day? 
+#What percentage of the requests were not successful (any 4xx status code)? DONE
+#What percentage of the requests were redirected elsewhere (any 3xx codes)? DONE
 #What was the most-requested file?
 #What was the least-requested file?
